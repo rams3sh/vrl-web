43e2a2b9dcbd518000a1c0699d3ce83558db2b7d
Shaeq Ahmed <shaeq.ahmed@gmail.com>
2022-12-28 03:00:15 -0800

    make test server use mutex to avoid slow to recompile long VRL programs being annoying while just testing (completely thread local runtimes in prod)

--- vrl-web-server-warp/src/resolve.rs
+++ vrl-web-server-warp/src/resolve.rs
@@ -6,9 +6,12 @@ use vector_common::TimeZone;
 use vrl::{diagnostic::Formatter, state, value, Program, Runtime, TargetValueRef};
 use warp::{reply::json, Reply};
 
-use log::{debug, error, info, warn};
 use anyhow::{anyhow, Result};
+use lazy_static::lazy_static;
+use log::{debug, error, info, warn};
 use lru::LruCache;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::{cell::RefCell, time::Instant};
 
 thread_local! {
@@ -33,97 +36,97 @@ enum Outcome {
 
 // The VRL resolution logic
 fn resolve(input: Input) -> Outcome {
-    thread_local!(
-        static CACHE: RefCell<LruCache<String, Result<Program, String>>> =
-            RefCell::new(LruCache::new(std::num::NonZeroUsize::new(400).unwrap()));
-    );
+    lazy_static! {
+        static ref CACHE: Arc<Mutex<LruCache<String, Result<Program, String>>>> = Arc::new(
+            Mutex::new(LruCache::new(std::num::NonZeroUsize::new(400).unwrap()))
+        );
+    };
 
     let mut value: Value = input.event.unwrap_or(value!({}));
     let program = input.program.as_str();
 
-    let res = CACHE.with(|c| {
-        let mut cache_ref = c.borrow_mut();
-        let stored_result = (*cache_ref).get(program);
+    let mut cache_ref = CACHE.lock().unwrap();
 
-        let start = Instant::now();
-        let compiled = match stored_result {
-            Some(compiled) => match compiled {
-                Ok(compiled) => Ok(compiled),
-                Err(e) => {
-                    return Err(anyhow!(e.clone()));
-                }
-            },
-            None => match vrl::compile(program, &vrl_stdlib::all()) {
-                Ok(result) => {
-                    debug!(
-                        "Compiled a vrl program ({}), took {:?}",
-                        program
-                            .lines()
-                            .into_iter()
-                            .skip(1)
-                            .next()
-                            .unwrap_or("expansion"),
-                        start.elapsed()
-                    );
-                    (*cache_ref).put(program.to_string(), Ok(result.program));
-                    if result.warnings.len() > 0 {
-                        warn!("{:?}", result.warnings);
-                    }
-                    match (*cache_ref).get(program) {
-                        Some(compiled) => match compiled {
-                            Ok(compiled) => Ok(compiled),
-                            Err(e) => {
-                                return Err(anyhow!(e.clone()));
-                            }
-                        },
-                        None => unreachable!(),
-                    }
-                }
-                Err(diagnostics) => {
-                    let msg = Formatter::new(&program, diagnostics).to_string();
-                    (*cache_ref).put(program.to_string(), Err(msg.clone()));
-                    Err(anyhow!(msg))
-                }
-            },
-        };
+    let stored_result = (*cache_ref).get(program);
 
-        if compiled.is_err() {
-            return Ok(Outcome::Error(compiled.err().unwrap().to_string()));
-        }
-        let compiled = compiled.unwrap();
-
-        let mut metadata = ::value::Value::Object(BTreeMap::new());
-        let mut secrets = ::value::Secrets::new();
-        let mut target = TargetValueRef {
-            value: &mut value,
-            metadata: &mut metadata,
-            secrets: &mut secrets,
-        };
-
-        let time_zone_str = Some("tt".to_string()).unwrap_or_default();
-
-        let time_zone = match TimeZone::parse(&time_zone_str) {
-            Some(tz) => tz,
-            None => TimeZone::Local,
-        };
-
-        let result = RUNTIME.with(|r| {
-            let mut runtime = r.borrow_mut();
-
-            match (*runtime).resolve(&mut target, &compiled, &time_zone) {
-                Ok(result) => Ok(result),
-                Err(err) => Err(err.to_string()),
+    let start = Instant::now();
+    let compiled = match stored_result {
+        Some(compiled) => match compiled {
+            Ok(compiled) => Ok(compiled),
+            Err(e) => {
+                return Outcome::Error(e.clone());
             }
-        });
+        },
+        None => match vrl::compile(program, &vrl_stdlib::all()) {
+            Ok(result) => {
+                debug!(
+                    "Compiled a vrl program ({}), took {:?}",
+                    program
+                        .lines()
+                        .into_iter()
+                        .skip(1)
+                        .next()
+                        .unwrap_or("expansion"),
+                    start.elapsed()
+                );
+                (*cache_ref).put(program.to_string(), Ok(result.program));
+                if result.warnings.len() > 0 {
+                    warn!("{:?}", result.warnings);
+                }
+                match (*cache_ref).get(program) {
+                    Some(compiled) => match compiled {
+                        Ok(compiled) => Ok(compiled),
+                        Err(e) => {
+                            return Outcome::Error(e.clone());
+                        }
+                    },
+                    None => unreachable!(),
+                }
+            }
+            Err(diagnostics) => {
+                let msg = Formatter::new(&program, diagnostics).to_string();
+                (*cache_ref).put(program.to_string(), Err(msg.clone()));
+                Err(anyhow!(msg))
+            }
+        },
+    };
 
-        match result {
-            Ok(result) => Ok(Outcome::Success {
-                output: result,
-                result: value,
-            }),
-            Err(err) => Ok(Outcome::Error(err)),
+    if compiled.is_err() {
+        return Outcome::Error(compiled.err().unwrap().to_string());
+    }
+    let compiled = compiled.unwrap();
+
+    let mut metadata = ::value::Value::Object(BTreeMap::new());
+    let mut secrets = ::value::Secrets::new();
+    let mut target = TargetValueRef {
+        value: &mut value,
+        metadata: &mut metadata,
+        secrets: &mut secrets,
+    };
+
+    let time_zone_str = Some("tt".to_string()).unwrap_or_default();
+
+    let time_zone = match TimeZone::parse(&time_zone_str) {
+        Some(tz) => tz,
+        None => TimeZone::Local,
+    };
+
+    let result = RUNTIME.with(|r| {
+        let mut runtime = r.borrow_mut();
+
+        match (*runtime).resolve(&mut target, &compiled, &time_zone) {
+            Ok(result) => Ok(result),
+            Err(err) => Err(err.to_string()),
         }
-    }).unwrap();
+    });
+
+    let res = match result {
+        Ok(result) => Outcome::Success {
+            output: result,
+            result: value,
+        },
+        Err(err) => Outcome::Error(err),
+    };
 
     res
 }
